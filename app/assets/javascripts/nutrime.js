41a53ffcebf03e4f7d00f72f08be59a8759db91d
ZoharLiran <zoharliran@gmail.com>
2015-11-26 20:51:13 -0800

    Define angular app and require it

---
+++ app/assets/javascripts/nutrime.js
@@ -0,0 +1,10 @@
+App = angular.module('Nutrime', ['mobile-angular-ui']);
+
+App.config(function($httpProvider) {
+  var csrfToken = document.getElementsByName('csrf-token')[0];
+  if(csrfToken) {
+    $httpProvider.defaults.headers.common['X-CSRF-Token'] = csrfToken.content;
+  }
+
+  $httpProvider.defaults.headers.common.Accept = 'application/json';
+});
